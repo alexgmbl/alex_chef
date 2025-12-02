8140854e8c88d6dec92bf5340f2c3e2d46b6572c
AlexXCode <oleksandr.delapay@gmail.com>
2025-12-02 10:37:16 +0100

    Change build command to use xcodeproj instead of xcworkspace

---
+++ Alex_chef_Demo2/Alex_chef_Demo2App.swift
@@ -0,0 +1,17 @@
+//
+//  Alex_chef_Demo2App.swift
+//  Alex_chef_Demo2
+//
+//  Created by Oleksandr Kononskyi on 02.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct Alex_chef_Demo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alex_chef_Demo2Tests/Alex_chef_Demo2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Alex_chef_Demo2Tests.swift
+//  Alex_chef_Demo2Tests
+//
+//  Created by Oleksandr Kononskyi on 02.12.2025.
+//
+
+import Testing
+@testable import Alex_chef_Demo2
+
+struct Alex_chef_Demo2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
